f23b96e9ce74658124087b1c753637d478a63d2f
NamdevNikhil <namdevnikhil003@gmail.com>
2024-09-30 15:16:48 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -6,7 +6,12 @@ var isValid = function(s) {
     const st = [];
 
     for(let i of st){
-        if(i === '(' or i === '{' or i ==='[')
+        if(i === '(' or i === '{' or i ==='['){
+            st.push(i);
+        }
+        else{
+            
+        }
     }
     return st.length === 0;
 };
