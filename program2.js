0d290d968bc03f24d6b8b205cc6a5c4f71d991c4
NamdevNikhil <namdevnikhil003@gmail.com>
2024-09-30 15:30:34 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,17 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const 
+    const roman = {
+        'I' : 1,
+        'v' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000
+    };
+
+    let ans = 0;
 };
 
 
